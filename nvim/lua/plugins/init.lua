e26cbb0c2bcee2ecde28ee0ffbdee18167209017
Peter Rasmussen <pmrrasmussen@icloud.com>
2024-11-27 06:09:47 +0100

    Added kickstart

--- nvim/lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { import = "plugins.editor" },
-  { import = "plugins.git" },
-  { import = "plugins.lang" },
-  { import = "plugins.lsp" },
-  { import = "plugins.navigation" },
-	{ import = "plugins.colorschemes" },
-	{ import = "plugins.ui" },
-}
--- nvim/lua/plugins/navigation/telescope-tmux.lua
+++
@@ -1,8 +0,0 @@
-return {
-	-- Telescope tmux (telescope extension for tmux)
-	"camgraff/telescope-tmux.nvim",
-	keys = {
-		{ "<Leader>ts", ":Telescope tmux sessions<CR>", mode = { "n", "v" }, desc = "Search tmux sessions" },
-		{ "<Leader>tw", ":Telescope tmux windows<CR>", mode = { "n", "v" }, desc = "Search tmux windows" },
-	},
-}
