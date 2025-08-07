c0a73829d16fe205a47ebd0983ef31fb95b8ae28
Peter Rasmussen <pr@twig.energy>
2025-08-07 15:55:13 +0000

    small improvements to setup and tmux

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 rm .zsh*
 rm .zcompdump
 git clone https://github.com/pmrrasmussen/dotfiles.git
+mkdir -p .config
 ln -s $HOME/dotfiles/zsh/.zshenv .zshenv
+ln -s $HOME/dotfiles/.tmux.conf .tmux.conf
+ln -s $HOME/dotfiles/nvim .config/nvim
