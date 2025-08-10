bfaf0f351091be3feed5039c205582615880ab49
Peter Rasmussen <pmrrasmussen@icloud.com>
2025-08-10 19:59:42 +0200

    lsp magic

--- nvim/lua/plugins/editor/mini.lua
+++ nvim/lua/plugins/editor/mini.lua
@@ -2,7 +2,6 @@ return {
 	"echasnovski/mini.nvim",
 	version = false,
 	config = function()
-		-- require("mini.pairs").setup()
 		require("mini.move").setup()
 		require("mini.splitjoin").setup()
 	end,
