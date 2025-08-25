35850e34fd65c17966d4923459ccee72544086b5
Peter Rasmussen <pr@twig.energy>
2025-08-25 06:49:51 +0000

    improve various things

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -1,19 +1,20 @@
+vim.diagnostic.config({ virtual_text = true })
 return {
-	{ "mason-org/mason.nvim", opts = {} },
+    { "mason-org/mason.nvim", opts = {} },
 
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "neovim/nvim-lspconfig" },
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"jsonls",
-				"clangd",
-				"pyright",
-				"ruff",
-				"yamlls",
-			},
-			automatic_installation = true,
-		},
-	},
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = { "neovim/nvim-lspconfig" },
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "jsonls",
+                "clangd",
+                "pyright",
+                "ruff",
+                "yamlls",
+            },
+            automatic_installation = true,
+        },
+    },
 }
