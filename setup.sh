f87b3e12e07a8267dee836816a49204b91bb91d0
Peter Rasmussen <pr@twig.energy>
2025-08-29 12:16:23 +0000

    fzf and tmux start

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ git submodule update --init --recursive
 cd $HOME
 mkdir -p .config
 ln -s $HOME/dotfiles/zsh/.zshenv .zshenv
-ln -s $HOME/dotfiles/.tmux.conf .tmux.conf
+ln -s $HOME/dotfiles/tmux/tmux.conf .tmux.conf
 ln -s $HOME/dotfiles/nvim .config/nvim
