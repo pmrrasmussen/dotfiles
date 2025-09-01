aa5323cffbd979d57e3461f02f7988083abc45ab
Peter Rasmussen <pr@twig.energy>
2025-09-01 07:21:31 +0000

    some setup changes

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 cd $HOME
-rm .zsh*
-rm .zcompdump
+rm -f .zshrc
+rm -f .zshenv
+rm -f .zcompdump
+rm -f .tmux.conf
 git clone https://github.com/pmrrasmussen/dotfiles.git
 cd dotfiles
 git submodule update --init --recursive
 cd $HOME
 mkdir -p .config
-ln -s $HOME/dotfiles/zsh/.zshenv .zshenv
+ln -s $HOME/dotfiles/zsh/zshenv .zshenv
 ln -s $HOME/dotfiles/tmux/tmux.conf .tmux.conf
 ln -s $HOME/dotfiles/nvim .config/nvim
