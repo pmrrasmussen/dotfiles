029e8f17190ae961d296cea2d998f1299bf42d4b
Peter Rasmussen <pr@twig.energy>
2025-10-03 09:33:49 +0000

    copilot improve

--- nvim/lua/plugins/ai/copilot.lua
+++ nvim/lua/plugins/ai/copilot.lua
@@ -1,5 +1,18 @@
 return {
-	{
-		"github/copilot.vim",
+	"zbirenbaum/copilot.lua",
+	event = "InsertEnter",
+	commit = "4725916b1e08a0cfed8fa6d9691ccd1609a347ee",
+	opts = {
+		suggestion = {
+			auto_trigger = true,
+			keymap = {
+				accept = "<M-j>",
+				accept_line = "<M-l>",
+				accept_word = "<M-k>",
+				next = "<M-]>",
+				prev = "<M-[>",
+				dismiss = "<M-c>",
+			},
+		},
 	},
 }
