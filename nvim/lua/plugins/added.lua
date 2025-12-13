dfbac977f27926808bce0e4032e71e2c4b8e7d20
Peter Rasmussen <pmrrasmussen@icloud.com>
2025-12-13 14:53:42 +0100

    fix some setup and add mini-nvim

--- nvim/lua/plugins/added.lua
+++ nvim/lua/plugins/added.lua
@@ -37,4 +37,12 @@ return {
     "neovim/nvim-lspconfig",
     opts = { diagnostics = { virtual_text = false } },
   },
+  {
+    "nvim-mini/mini.nvim",
+    version = false,
+    config = function()
+      require("mini.move").setup()
+      require("mini.splitjoin").setup()
+    end,
+  },
 }
