dcf4ed9e0adbc785893615ddb133d95a4e47dc80
Peter Rasmussen <pmrrasmussen@icloud.com>
2025-12-25 23:09:54 +0100

    add iron nvim

--- nvim/lua/plugins/added.lua
+++ nvim/lua/plugins/added.lua
@@ -41,4 +41,66 @@ return {
       require("mini.splitjoin").setup()
     end,
   },
+  {
+    "hkupty/iron.nvim",
+    config = function()
+      local iron = require("iron.core")
+
+      iron.setup({
+        config = {
+          -- Whether a repl should be discarded or not
+          scratch_repl = true,
+          -- Your repl definitions come here
+          repl_definition = {
+            sh = {
+              -- Can be a table or a function that
+              -- returns a table (see below)
+              command = { "zsh" },
+            },
+          },
+          -- How the repl window will be displayed
+          -- See below for more information
+          repl_open_cmd = require("iron.view").bottom(20),
+        },
+        -- Iron doesn't set keymaps by default anymore.
+        -- You can set them here or manually add keymaps to the functions in iron.core
+        keymaps = {
+          visual_send = "<leader>rv",
+          send_file = "<leader>rf",
+          send_line = "<leader>rl",
+          interrupt = "<leader>ri",
+          exit = "<leader>rq",
+          clear = "<leader>cl",
+        },
+        -- If the highlight is on, you can change how it looks
+        -- For the available options, check nvim_set_hl
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      })
+    end,
+    keys = {
+      {
+        "<leader>rs",
+        "<cmd>IronRepl<cr>",
+        desc = "(R)EPL (s)tart",
+      },
+      {
+        "<leader>rr",
+        "<cmd>IronRestart<cr>",
+        desc = "(R)EPL (r)estart",
+      },
+      {
+        "<leader>rf",
+        "<cmd>IronFocus<cr>",
+        desc = "(R)EPL (f)ocus",
+      },
+      {
+        "<leader>rh",
+        "<cmd>IronHide<cr>",
+        desc = "(R)EPL (h)ide",
+      },
+    },
+  },
 }
