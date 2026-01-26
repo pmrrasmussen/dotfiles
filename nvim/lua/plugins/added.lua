cdf62452768dee541872eb76572f11e74273efef
Peter Rasmussen <pr@twig.energy>
2026-01-26 15:05:45 +0100

    fix bug

--- nvim/lua/plugins/added.lua
+++ nvim/lua/plugins/added.lua
@@ -7,7 +7,7 @@ return {
       vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
       vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
       vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-      vim.keymae.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+      vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
     end,
   },
   {
